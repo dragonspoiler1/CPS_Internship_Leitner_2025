6165b0cd815578b2d3f87235212f38adfe4d13c0
220086 <156812453+220086@users.noreply.github.com>
2025-08-28 16:37:13 +0200

    overhaul

--- Programms/main.py
+++ Programms/main.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env pybricks-micropython
+
 from pybricks.hubs import EV3Brick
-from pybricks.parameters import Port
-from pybricks.tools import wait
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import (Motor, ColorSensor, TouchSensor, UltrasonicSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.messaging import BluetoothMailboxClient, TextMailbox
+
+def middleButtonIsPressed(ev3):
+    # Returns True if the center button of the EV3 brick is currently pressed.
+    return Button.CENTER in ev3.buttons.pressed()
 
 ev3 = EV3Brick()
-motorA = Motor(Port.A)
-motorB = Motor(Port.B)
+COLORS = [Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW]
+colorindex = 0
 
 
-motorA.run(160)
-motorB.run(160)
+
+
+while True:
+    if(middleButtonIsPressed(ev3) == True):
+        if(colorindex >=3):
+                colorindex = 0
+        else:
+                colorindex=colorindex+1
+            
+        ev3.screen.print(COLORS[colorindex])
+
+    wait(500)
