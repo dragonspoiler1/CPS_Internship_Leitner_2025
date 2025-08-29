4d6bd344804b36f1c847712a9425ecb317ecc4bc
220086 <156812453+220086@users.noreply.github.com>
2025-08-29 13:53:14 +0200

    last changes

--- Programms/main.py
+++ Programms/main.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env pybricks-micropython
-
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, ColorSensor, TouchSensor, UltrasonicSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.messaging import BluetoothMailboxClient, TextMailbox
+from utime import ticks_ms, ticks_diff
 
-def middleButtonIsPressed(ev3):
-    # Returns True if the center button of the EV3 brick is currently pressed.
-    return Button.CENTER in ev3.buttons.pressed()
 
 ev3 = EV3Brick()
-COLORS = [Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW]
-colorindex = 0
 
+sorterMotor = Motor(Port.B)
+sorterColorSensor = ColorSensor(Port.S3)
+colorToBeFiltered = Color.BLUE
 
-
+liftMotor = Motor(Port.A)
 
 while True:
-    if(middleButtonIsPressed(ev3) == True):
-        if(colorindex >=3):
-                colorindex = 0
-        else:
-                colorindex=colorindex+1
-            
-        ev3.screen.print(COLORS[colorindex])
-
-    wait(500)
+    color = sorterColorSensor.color()
+    if (color == colorToBeFiltered and not None):
+        print(color)
+        
+    wait(100)
